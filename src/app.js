5b0c38566c9f7b75b8e6b4e233453f811347fe7c
josemiguelhseptien <josemiguelhseptien@gmail.com>
2022-06-17 00:35:39 +0000

    save to submit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,21 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { arrow } from "@popperjs/core";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let output = [];
+
+  for (let value1 of pronoun) {
+    for (let value2 of adj) {
+      for (let value3 of noun) {
+        console.log(value1.concat(value2.concat(value3)));
+      }
+    }
+  }
 };
